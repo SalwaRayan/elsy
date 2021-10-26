5435adeff32a53ccb480b1c78c12606c24f5501a
SalwaRayan <90609792+SalwaRayan@users.noreply.github.com>
2021-10-26 09:35:35 +0200

    completed part 1

--- src/components/Box.js
+++ src/components/Box.js
@@ -5,7 +5,7 @@ class Steps extends React.Component {
     return (
       <div className="box col-sm-3 col-6">
         <span class="material-icons" style={{fontSize: 100, color: this.props.color}}>{this.props.icon}</span>
-        <p>{this.props.value} : {this.props.unit}</p>
+        <p>{this.props.value} {this.props.unit}</p>
       </div>
     )
   }
